cff89a5e0a0de7f9ebadb4886c30d3437c7fa08e
Marvin Alegre <marvinalegredev@gmail.com>
2026-09-18 16:30:20 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$title = "PHP Test";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+<body>
+    <main>
+	<h1><?= $title ?></h1>
+        <p>PHP is working.</p>
+    </main>
+</body>
+</html>
