b71fb91437602d8497bc1ff19f5341d74b7f043a
Marvin Alegre <marvinalegredev@gmail.com>
2026-09-19 09:11:52 +0800

    enable foreign keys

--- src/db.php
+++ src/db.php
@@ -7,6 +7,7 @@ function db(): PDO
     if ($db === null) {
         $db = new PDO("sqlite:" . __DIR__ . "/../data/message.db");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->exec("PRAGMA foreign_keys = ON");
     }
 
     return $db;
